e7b59ae4a1de07e5d72ae45411e1783149230708
jiubei0408 <757795210@qq.com>
2020-07-12 12:00:03 +0800

    添加 notify-list 模块

---
+++ src/components/page-item/notify-list.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <div class="tableTitle">
+            <b>
+                通知列表 <span v-if="data.length > 0">共 {{data.length}} 条</span></b>
+        </div>
+        <el-card shadow="hover">
+            <el-table :data="data">
+            </el-table>
+        </el-card>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'notify-list',
+        data() {
+            return {
+                data: []
+            }
+        },
+        methods: {
+            refreshData() {
+
+            }
+        }
+    }
+</script>
+
+<style>
+    .tableTitle {
+        text-align: center;
+        font-size: 20px;
+        padding-bottom: 10px;
+    }
+</style>
--- src/components/page/index.vue
+++ src/components/page/index.vue
@@ -1,5 +1,16 @@
 <template>
-  <h1>
-    test1
-  </h1>
+    <div>
+        <notify-list/>
+    </div>
 </template>
+
+<script>
+    import NotifyList from "@/components/page-item/notify-list"
+    export default {
+        name: 'index',
+        components: {
+            'notify-list': NotifyList,
+        }
+    }
+
+</script>
